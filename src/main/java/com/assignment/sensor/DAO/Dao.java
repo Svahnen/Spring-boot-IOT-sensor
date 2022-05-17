2e6922f8bc53315c0746295618b8f7588faad13d
husseinkareem <husseinkeldoon@yahoo.com>
2022-05-17 14:10:57 +0200

    work on getlatesttemp

--- src/main/java/com/assignment/sensor/DAO/Dao.java
+++ src/main/java/com/assignment/sensor/DAO/Dao.java
@@ -3,11 +3,18 @@ package com.assignment.sensor.DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Dao {
     Connection con;
 
     public void connect() throws SQLException {
-        con = DriverManager.getConnection("jdbc:mariadb:localhost/data?user=grupp&password=iot");
+        con = DriverManager.getConnection("jdbc:mariadb:100.107.10.54/data?user=grupp&password=iot");
+    }
+
+    public void getLatestTemp() throws SQLException {
+        var query = "SELECT id, value, timestamp FROM temperature ORDER BY timestamp DESC LIMIT 1";
+        Statement stmt = con.prepareStatement(query);
+        try (ResultSet)
     }
 }
