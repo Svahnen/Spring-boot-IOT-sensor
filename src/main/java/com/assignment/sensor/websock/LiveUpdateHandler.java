745cccb28207fbc49d91705b6ad1c8895d89a936
Aldo Ortiz <Aldo.ortiz@live.se>
2022-05-18 11:39:50 +0200

    Hello Websocket

---
+++ src/main/java/com/assignment/sensor/websock/LiveUpdateHandler.java
@@ -0,0 +1,42 @@
+package com.assignment.sensor.websock;
+
+import com.assignment.sensor.Modules.Temp;
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+// shamelessly stolen without credit
+
+@Component
+public class LiveUpdateHandler extends TextWebSocketHandler {
+    static Gson g = new Gson();
+
+    //Thread safe list
+    List <WebSocketSession>sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        //the messages will be broadcasted to all users.
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+
+    public void sendToAll(Temp temp) throws InterruptedException, IOException {
+        var foo = new TextMessage(g.toJson(temp));
+        for(WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(foo);
+        }
+    }
+}
