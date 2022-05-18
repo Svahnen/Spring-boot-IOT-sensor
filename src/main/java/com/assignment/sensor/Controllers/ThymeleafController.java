fb12a704740b5bf8a778e8352f533f5b78ff422d
Svahnen <svahn.jonny@gmail.com>
2022-05-18 15:13:56 +0200

    Handle sql disconnect

--- src/main/java/com/assignment/sensor/Controllers/ThymeleafController.java
+++ src/main/java/com/assignment/sensor/Controllers/ThymeleafController.java
@@ -1,11 +1,13 @@
 package com.assignment.sensor.Controllers;
 
 import java.sql.SQLException;
+import java.sql.SQLNonTransientConnectionException;
 
 import com.assignment.sensor.DAO.Dao;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +22,10 @@ public class ThymeleafController {
         model.addAttribute("temps", dao.getHistory(25, c));
 
         return "index";
+    }
+
+    @ExceptionHandler(SQLNonTransientConnectionException.class)
+    public void handleSQLDisconnect() throws SQLException {
+        dao.connect();
     }
 }
